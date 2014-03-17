3527c461bc98be46df27637e5e5dbe8e9858c8da
ugurersoy <ugurersoy41@gmail.com>
2014-03-17 23:38:49 +0200

    Parametreler eklendi.

--- src/com/acme/customization/MessageTemplateHandler.java
+++ src/com/acme/customization/MessageTemplateHandler.java
@@ -1,11 +1,39 @@
 package com.acme.customization;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.axis2.databinding.types.soapencoding.Array;
+
+import com.acme.enums.Parameters;
+import com.lbs.grids.JLbsObjectListGrid;
+import com.lbs.xui.JLbsXUIPane;
 import com.lbs.xui.events.swing.JLbsCustomXUIEventListener;
+import com.lbs.xui.customization.JLbsXUIControlEvent;
 
 public class MessageTemplateHandler extends JLbsCustomXUIEventListener {
-
+  private Parameters parameter[];
+	
 	public MessageTemplateHandler() {
 		// TODO Auto-generated constructor stub
 	}
+
+	public void onInitialize(JLbsXUIControlEvent event)
+	{
+		JLbsXUIPane container = event.getContainer();
+		
+		JLbsObjectListGrid objectGrid= (JLbsObjectListGrid) container.getComponentByTag(3000005);
+	 
+		ArrayList<String> parameterList=new ArrayList();
+		
+		for(Parameters param : parameter)
+		{
+			parameterList.add(param.getParameterName());
+		}
+
+		objectGrid.setObjectClass(parameterList.getClass());
+	
+	}
+	
 
 }
