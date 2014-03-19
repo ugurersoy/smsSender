4b4616403fd75bd4607f3e7edf9c6866f0e2aed9
ugurersoy <ugurersoy41@gmail.com>
2014-03-19 22:11:04 +0200

    paramatre test

--- src/com/acme/customization/MessageTemplateHandler.java
+++ src/com/acme/customization/MessageTemplateHandler.java
@@ -3,16 +3,19 @@ package com.acme.customization;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 import org.apache.axis2.databinding.types.soapencoding.Array;
 
 import com.acme.enums.Parameters;
+import com.lbs.controls.maskededit.JLbsTextEdit;
 import com.lbs.grids.JLbsObjectListGrid;
 import com.lbs.xui.JLbsXUIPane;
 import com.lbs.xui.events.swing.JLbsCustomXUIEventListener;
 import com.lbs.xui.customization.JLbsXUIControlEvent;
 
 public class MessageTemplateHandler extends JLbsCustomXUIEventListener {
-  private Parameters parameter[];
+  private Parameters parameter[]=Parameters.values();
 	
 	public MessageTemplateHandler() {
 		// TODO Auto-generated constructor stub
@@ -22,15 +25,14 @@ public class MessageTemplateHandler extends JLbsCustomXUIEventListener {
 	{
 		JLbsXUIPane container = event.getContainer();
 		
-		JLbsObjectListGrid objectGrid= (JLbsObjectListGrid) container.getComponentByTag(3000005);
-	 
-		ArrayList<String> parameterList=new ArrayList();
-		
+		ArrayList parameterList=new ArrayList();
+	
 		for(Parameters param : parameter)
 		{
 			parameterList.add(param.getParameterName());
 		}
-
+		
+		JLbsObjectListGrid objectGrid= (JLbsObjectListGrid) container.getComponentByTag(3000005);
 		objectGrid.setObjectClass(parameterList.getClass());
 	
 	}
