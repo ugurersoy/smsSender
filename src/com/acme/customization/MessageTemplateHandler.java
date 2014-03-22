f190d1d0c44c36f5d099780ae7a224455741668c
ugurersoy <ugurersoy41@gmail.com>
2014-03-22 11:55:27 +0200

    parameters complated in messageTemplate

--- src/com/acme/customization/MessageTemplateHandler.java
+++ src/com/acme/customization/MessageTemplateHandler.java
@@ -7,56 +7,91 @@ import javax.swing.JOptionPane;
 import com.acme.entity.entityParameter;
 import com.acme.enums.Parameters;
 import com.lbs.controls.JLbsEditorPane;
+import com.lbs.controls.buttonpanel.JLbsPrefixPanel;
+import com.lbs.controls.maskededit.JLbsTextEdit;
 import com.lbs.grids.JLbsObjectListGrid;
 import com.lbs.xui.JLbsXUIPane;
 import com.lbs.xui.customization.JLbsXUIControlEvent;
 import com.lbs.xui.events.swing.JLbsCustomXUIEventListener;
+import com.lbs.xui.customization.JLbsXUIGridEvent;
 
 public class MessageTemplateHandler extends JLbsCustomXUIEventListener {
-  private Parameters parameter[]=Parameters.values();
-	
+	private Parameters parameter[] = Parameters.values();
+
 	public MessageTemplateHandler() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public void onInitialize(JLbsXUIControlEvent event)
-	{
+	public void onInitialize(JLbsXUIControlEvent event) {
 		JLbsXUIPane container = event.getContainer();
-		
-		
-		ArrayList parameterList=new ArrayList();
-	
-		
-		for(Parameters param : parameter)
-		{
+
+		ArrayList parameterList = new ArrayList();
+
+		for (Parameters param : parameter) {
 			parameterList.add(new entityParameter(param.getParameterName()));
 		}
-		
-		JLbsObjectListGrid objectGrid= (JLbsObjectListGrid) container.getComponentByTag(3000005);
+
+		JLbsObjectListGrid objectGrid = (JLbsObjectListGrid) container
+				.getComponentByTag(3000005);
 		objectGrid.setObjectClass(parameterList.getClass());
 		objectGrid.setObjects(parameterList);
-	
+
 	}
 
-	public void parameterOnClick(JLbsXUIControlEvent event)
-	{
-		JOptionPane.showMessageDialog(null, "test");
-//		JLbsXUIPane container = event.getContainer();
-//		
-//		JLbsEditorPane messageTemplate = (JLbsEditorPane) container.getComponentByTag(3000002);
-//		JLbsObjectListGrid objectGrid= (JLbsObjectListGrid) container.getComponentByTag(3000005);
-//		
-//        ArrayList list = (ArrayList) objectGrid.getDisplayList();
-//	
-//        for(int i=0;i<list.size();i++)
-//        {
-//        if(objectGrid.getSelectedRow()==list.indexOf(i))
-//           {
-//        	entityParameter param = (entityParameter) list.get(i); 
-//        	messageTemplate.setText(param.parameter);
-//           }
-//        }
+	public void parameterOnClick(JLbsXUIControlEvent event) {
+		JLbsXUIPane container = event.getContainer();
+
+		JLbsTextEdit messageTemplate = (JLbsTextEdit) container
+				.getComponentByTag(3000007);
+
+		JLbsObjectListGrid objectGrid = (JLbsObjectListGrid) container
+				.getComponentByTag(3000005);
+
+		ArrayList list = (ArrayList) objectGrid.getDisplayList();
+
+		for (int i = 0; i < list.size(); i++) {
+			if (list.indexOf(list.get(i)) == objectGrid.getSelectedRow()) {
+				entityParameter param = (entityParameter) list.get(i);
+				if(!messageTemplate.getText().isEmpty())
+				{
+					messageTemplate.setText(messageTemplate.getText()+" "+param.parameter);
+				}
+				else 
+				messageTemplate.setText(param.parameter);
+				continue;
+			}
+		}
+
 	}
-	
+
+	public void callParameter(JLbsXUIGridEvent event) {
+
+	}
+
+	public void selectParameterDoubleClick(JLbsXUIGridEvent event)
+	{
+		JLbsXUIPane container = event.getContainer();
+
+		JLbsTextEdit messageTemplate = (JLbsTextEdit) container
+				.getComponentByTag(3000007);
+
+		JLbsObjectListGrid objectGrid = (JLbsObjectListGrid) container
+				.getComponentByTag(3000005);
+
+		ArrayList list = (ArrayList) objectGrid.getDisplayList();
+
+		for (int i = 0; i < list.size(); i++) {
+			if (list.indexOf(list.get(i)) == objectGrid.getSelectedRow()) {
+				entityParameter param = (entityParameter) list.get(i);
+				if(!messageTemplate.getText().isEmpty())
+				{
+					messageTemplate.setText(messageTemplate.getText()+" "+param.parameter);
+				}
+				else 
+				messageTemplate.setText(param.parameter);
+				continue;
+			}
+		}
+	}
 
 }
